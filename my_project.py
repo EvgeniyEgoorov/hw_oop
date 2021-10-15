8b8eba145e90c9c96e2449e93463f1d1d9f051da
Evgeniy Egorov <egorovzhenya@gmail.com>
2021-10-15 18:12:13 +0400

    Lecturer rate added

--- my_project.py
+++ my_project.py
@@ -7,6 +7,16 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecture(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached \
+                and course in self.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -14,6 +24,14 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -24,22 +42,25 @@ class Mentor:
             return 'Ошибка'
 
 
-class Lecturer(Mentor):
-    pass
-
-
-class Reviewer(Mentor):
-    pass
-
-
-best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student = Student('Roy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
+cool_mentor = Reviewer('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
 
+best_mentor = Lecturer('Some', 'Buddy')
+best_mentor.courses_attached += ['Git']
+
+cool_student = Student('Roy', 'Eman', 'your_gender')
+cool_student.courses_in_progress += ['Git']
+
 cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
 
+cool_student.rate_lecture(best_mentor, 'Git', 10)
+cool_student.rate_lecture(best_mentor, 'Git', 10)
+cool_student.rate_lecture(best_mentor, 'Git', 10)
+#
 print(best_student.grades)
+print(best_mentor.grades)
