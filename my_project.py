a4d93ef62d0a585fea1cc7bdb63cd094d572bf00
Evgeniy Egorov <egorovzhenya@gmail.com>
2021-10-15 22:39:04 +0400

    comparison added

--- my_project.py
+++ my_project.py
@@ -21,6 +21,12 @@ class Student:
         for values in self.grades.values():
             return round(sum(values) / len(values), 1)
 
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            print('Пожалуйста, укажите имя студента')
+            return
+        return self.avg_rate() < other.avg_rate()
+
     def __str__(self):
         res = f'Имя: {self.name}\n' \
               f'Фамилия: {self.surname}\n' \
@@ -46,6 +52,12 @@ class Lecturer(Mentor):
         res = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {Student.avg_rate(self)}'
         return res
 
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            print('Пожалуйста, укажите имя лектора')
+            return
+        return Student.avg_rate(self) < Student.avg_rate(other)
+
 
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
@@ -62,29 +74,41 @@ class Reviewer(Mentor):
         return res
 
 
-some_student = Student('Some', 'Buddy', 'your_gender')
+some_student = Student('Tom', 'Jones', 'your_gender')
 some_student.courses_in_progress += ['Python']
 some_student.courses_in_progress += ['Git']
 some_student.finished_courses += ['Введение в программирование']
 
-some_reviewer = Reviewer('Some', 'Buddy')
-some_reviewer.courses_attached += ['Python']
+other_student = Student('Peter', 'Hunt', 'your_gender')
+other_student.courses_in_progress += ['Python']
+other_student.courses_in_progress += ['JS']
 
-some_lecturer = Lecturer('Some', 'Buddy')
+some_lecturer = Lecturer('Jake', 'Smith')
 some_lecturer.courses_attached += ['JS']
 
-other_student = Student('Some', 'Buddy', 'your_gender')
-other_student.courses_in_progress += ['JS']
+other_lecturer = Lecturer('Jo', 'Black')
+other_lecturer.courses_attached += ['Python']
+other_lecturer.courses_attached += ['JS']
+
+some_reviewer = Reviewer('Billy', 'Baxter')
+some_reviewer.courses_attached += ['Python']
 
 some_reviewer.rate_hw(some_student, 'Python', 9)
 some_reviewer.rate_hw(some_student, 'Python', 6)
 some_reviewer.rate_hw(some_student, 'Python', 10)
 
+some_reviewer.rate_hw(other_student, 'Python', 4)
+some_reviewer.rate_hw(other_student, 'Python', 5)
+some_reviewer.rate_hw(other_student, 'Python', 8)
+
 other_student.rate_lecture(some_lecturer, 'JS', 7)
 other_student.rate_lecture(some_lecturer, 'JS', 9)
 other_student.rate_lecture(some_lecturer, 'JS', 10)
-other_student.rate_lecture(some_lecturer, 'JS', 6)
-other_student.rate_lecture(some_lecturer, 'JS', 5)
+
+other_student.rate_lecture(other_lecturer, 'JS', 5)
+other_student.rate_lecture(other_lecturer, 'JS', 7)
+other_student.rate_lecture(other_lecturer, 'JS', 9)
+
 #
 print(f'Оценки студентов: {some_student.grades}', '\n')
 print(f'Оценки преподавателей: {some_lecturer.grades}', '\n')
@@ -92,3 +116,6 @@ print(f'Оценки преподавателей: {some_lecturer.grades}', '\n'
 print(some_reviewer, '\n')
 print(some_lecturer, '\n')
 print(some_student, '\n')
+
+print(some_student < other_student)
+print(some_lecturer > other_lecturer)
